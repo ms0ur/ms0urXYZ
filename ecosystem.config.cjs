8685db042689fec63f9a61347ec23f1f2bd7cf2b
ms0ur <maksimsuslov7142@gmail.com>
2025-08-13 00:56:08 +0300

    v.1.0

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'ms0urXYZ',
+            script: '.output/server/index.mjs',
+            env: {
+                PORT: 4000,
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+}
